eb41e2bbe32b01731c8a6b645cca524d0d469b9e
Mark Griffin <mrmg@deflexion.net>
2014-12-06 18:26:07 +0000

    Added dynamic lighting, not sure I like it Changed branching of classes

---
+++ Game/Assets/Scripts/CharacterBase.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterBase : MonoBehaviour {
+	public bool onGround = false;
+	public Light spotlight;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update() {
+		spotlight.transform.LookAt (gameObject.transform);
+	}
+	
+	void OnCollisionEnter (Collision col) {
+		if (col.gameObject.name == "Monitor" || col.gameObject.name == "Screen") {
+			onGround = true;
+		}
+	}
+}
--- Game/Assets/Scripts/PlayerController.cs
+++ Game/Assets/Scripts/PlayerController.cs
@@ -1,26 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CharacterBase : MonoBehaviour {
-	public bool onGround = false;
-	public Light spotlight;
-	
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update() {
-	}
-	
-	void OnCollisionEnter (Collision col) {
-		if (col.gameObject.name == "Monitor" || col.gameObject.name == "Screen") {
-			onGround = true;
-		}
-	}
-}
-
 public class PlayerController : CharacterBase {
 	public float maxSpeed = 5f;
 	public float jumpSpeed = 250f;
@@ -36,13 +16,9 @@ public class PlayerController : CharacterBase {
 	
 	// Update is called once per frame
 	void Update () {
-		//base.Update ();
-		spotlight.transform.LookAt (gameObject.transform);
-
 		float xMove = Input.GetAxis (horizontalControl);
 		float zMove = Input.GetAxis (verticalControl);
 		float yMove = 0f;
-
 
 		if (Input.GetButtonDown (jumpControl) && onGround) {
 			onGround = false;
