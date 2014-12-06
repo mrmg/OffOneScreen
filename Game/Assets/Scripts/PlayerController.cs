00dbadf93cf6ecb7b6816bd114db1375b9ce704b
Mark Griffin <mrmg@deflexion.net>
2014-12-06 13:11:42 +0000

    Updated graphics Added 2nd controller

--- Game/Assets/Scripts/PlayerController.cs
+++ Game/Assets/Scripts/PlayerController.cs
@@ -11,7 +11,10 @@ public class CharacterBase : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+		if (gameObject.rigidbody.position.y < -6) {
+			Debug.Log ("player died");
+		}
 	}
 	
 	void OnCollisionEnter (Collision col) {
@@ -25,6 +28,10 @@ public class PlayerController : CharacterBase {
 	public float maxSpeed = 5f;
 	public float jumpSpeed = 250f;
 
+	public string horizontalControl = "Horizontal";
+	public string verticalControl = "Vertical";
+	public string jumpControl = "Jump";
+
 	// Use this for initialization
 	void Start () {
 
@@ -32,12 +39,12 @@ public class PlayerController : CharacterBase {
 	
 	// Update is called once per frame
 	void Update () {
-		float xMove = Input.GetAxis ("Horizontal");
-		float zMove = Input.GetAxis ("Vertical");
+		float xMove = Input.GetAxis (horizontalControl);
+		float zMove = Input.GetAxis (verticalControl);
 		float yMove = 0f;
 
 
-		if (Input.GetKeyDown (KeyCode.Space) && onGround) {
+		if (Input.GetButtonDown (jumpControl) && onGround) {
 			onGround = false;
 			yMove = 1;
 		}
